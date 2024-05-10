aadcb39f7f60f880a92a93809dc8e58869f681f2
Ian Hart <ianhart7790@gmail.com>
2024-05-10 14:01:45 -0400

    feat: add project data to display

---
+++ src/data/projects.js
@@ -0,0 +1,57 @@
+import educationMangementImg from '../assets/kbi.png';
+import filmScoutImg from '../assets/film_scout.png';
+import passwordGeneratorImg from '../assets/password-generator.png';
+import horiseonImg from '../assets/horiseon.png';
+import workDaySchedulerImg from '../assets/scheduler.png';
+import weatherImg from '../assets/weather-dashboard.png';
+
+export const projectsData = [
+  {
+    id: 1,
+    name: 'Education Management',
+    tools: 'Express/mySQL',
+    github_url: 'https://github.com/ianahart/Education-Management-Application',
+    deployed_url: 'https://education-management-hart-7c088cb1e75a.herokuapp.com/',
+    image_src: educationMangementImg,
+  },
+  {
+    id: 2,
+    name: 'Film Scout',
+    tools: 'HTML/CSS/JavaScript',
+    github_url: 'https://github.com/ianahart/movie-data',
+    deployed_url: 'https://afry72.github.io/movie-data/',
+    image_src: filmScoutImg,
+  },
+  {
+    id: 3,
+    name: 'Password Generator',
+    tools: 'JavaScript',
+    github_url: 'https://github.com/ianahart/unique-password-generator',
+    deployed_url: 'https://ianahart.github.io/unique-password-generator/',
+    image_src: passwordGeneratorImg,
+  },
+  {
+    id: 4,
+    name: 'Horiseon',
+    tools: 'HTML/CSS',
+    github_url: 'https://github.com/ianahart/bc-horiseon',
+    deployed_url: 'https://ianahart.github.io/bc-horiseon/',
+    image_src: horiseonImg,
+  },
+  {
+    id: 5,
+    name: 'Scheduler',
+    tools: 'HTML/CSS/jQuery',
+    github_url: 'https://github.com/ianahart/work-day-scheduler',
+    deployed_url: 'https://ianahart.github.io/work-day-scheduler/',
+    image_src: workDaySchedulerImg,
+  },
+  {
+    id: 6,
+    name: 'Weather Dashboard',
+    tools: 'HTML/CSS/JavaScript',
+    github_url: 'https://github.com/ianahart/weather-dashboard',
+    deployed_url: 'https://ianahart.github.io/weather-dashboard/',
+    image_src: weatherImg,
+  },
+];
