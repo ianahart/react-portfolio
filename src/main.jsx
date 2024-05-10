fcf9c31f54494f1e63d00e9b0187226cc80f4cc8
Ian Hart <ianhart7790@gmail.com>
2024-05-10 11:37:15 -0400

    feat: setup router for all of the portfolio routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,43 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import NotFoundRoute from './routes/NotFoundRoute';
+import ResumeRoute from './routes/ResumeRoute';
+import PortfolioRoute from './routes/PortfolioRoute';
+import AboutRoute from './routes/AboutRoute';
+import ContactRoute from './routes/ContactRoute';
+
+import './index.css';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <NotFoundRoute />,
+    children: [
+      {
+        index: true,
+        element: <AboutRoute />,
+      },
+      {
+        path: '/portfolio',
+        element: <PortfolioRoute />,
+      },
+      {
+        path: '/contact',
+        element: <ContactRoute />,
+      },
+      {
+        path: '/resume',
+        element: <ResumeRoute />,
+      },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
